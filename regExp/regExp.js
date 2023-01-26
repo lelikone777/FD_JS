1927c25e3147f5a591dedeb34866b9cb3ba16607
lelikone777 <2257855@gmail.com>
2023-01-26 12:42:25 +0300

    regExp

---
+++ regExp/regExp.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// new RegExp('pattern', 'flags');
+// /pattern/f
+
+// const ans = prompt('Введите ваше имя');
+
+// const reg = /n/i;
+// i - регистр
+// g - глобальный
+// m -
+
+// \d - цифры
+// \w - слова/буквы
+// \s - пробелы
+
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+// console.log(ans.replace(reg));
+// const pass = prompt('Password');
+// console.log(pass.replace(/\./g, "*"));
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
+// const ans = prompt('Введите ваше имя');
+// const reg = /n/ig;
+// console.log(reg.test(ans));
+
+// const ans = prompt('Введите ваше число');
+// const reg = /\d/g;
+// console.log(ans.match(reg));
+
+// const str = 'My name is R2D2';
+// console.log(str.match(/\W/i));
+
+// \D
+// \W
+
